b7e544df49421cdc0cd5fd04276e904df68993b5
Inge Jørgensen <inge@manualdesign.no>
2009-08-12 15:40:49 +0200

    The name of the form field is now configurable

--- init.rb
+++ init.rb
@@ -1,6 +1,11 @@
+module Honeypot
+  class << self; attr_accessor :form_field; end
+  self.form_field ||= :email
+end
+
 ActionController::Base.before_filter do |controller|
   controller.instance_eval {
-    render :nothing => true unless params[:email].blank?
+    render :nothing => true unless params[Honeypot.form_field].blank?
   }
 end
 
@@ -8,8 +13,8 @@ class ActionView::Helpers::FormBuilder
   def honeypot
     %{
       <div class="attack_of_the_titans">
-        #{@template.label_tag(:email, "Don't fill in this field")}
-        #{@template.text_field_tag(:email)}
+        #{@template.label_tag(Honeypot.form_field, "Don't fill in this field")}
+        #{@template.text_field_tag(Honeypot.form_field)}
       </div>
     }
   end
